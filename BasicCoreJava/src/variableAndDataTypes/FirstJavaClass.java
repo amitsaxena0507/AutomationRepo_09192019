1b44d111317f917fcf088d212bd126493f22fecc
amitsaxena0507 <amitsaxena0507@gmail.com>
2019-09-24 21:50:46 +0530

    commiting first data variables

---
+++ BasicCoreJava/src/variableAndDataTypes/FirstJavaClass.java
@@ -0,0 +1,26 @@
+package variableAndDataTypes;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		//widely used data types
+		int a=10;//for integer value
+		char b='A';// for single character
+		boolean c = true;//for boolean values
+		double d = 6.89;//for decimal values
+		//class
+		String name = "Amit";//for string values ** String is not a datatype
+		//non primitve -obj,array and interface
+		//primitive data types : int,boolean,char,double,byte,short,long,double,float
+		//other data types:
+		//for numbers
+		byte x =10;
+		short y =45;
+		long z = 6796896;
+
+		//for decimal values
+		float f = 67.8f;
+		System.out.println(a);
+	}
+
+}
